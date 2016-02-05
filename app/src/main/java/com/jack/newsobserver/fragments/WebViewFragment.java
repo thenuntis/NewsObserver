75f57b4b542a3aaaf5b917a48ee0b6abaf5b0f46
thenuntis <nuntis.me@gmail.com>
2016-02-05 10:20:48 +0200

    webview bug

--- app/src/main/java/com/jack/newsobserver/fragments/WebViewFragment.java
+++ app/src/main/java/com/jack/newsobserver/fragments/WebViewFragment.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +32,8 @@ public class WebViewFragment extends Fragment {
     private ArrayList<String> mHistoryList;
     private WebView mWebView;
     private ProgressBar mWebViewBar;
+    private static final String SCROLL_VALUE = "scrollingPosition";
+    private static int mScrollPositionY;
 
     public WebViewFragment(){
     }
@@ -39,6 +42,8 @@ public class WebViewFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        Log.w("ONCREATE", "call" + savedInstanceState);
+
     }
 
     @Override
@@ -48,13 +53,21 @@ public class WebViewFragment extends Fragment {
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new myWebViewClient());
         mHistoryList = new ArrayList<>();
+        Log.w("ONcreateVIEW", "call" + savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
         } else {
             mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
         }
+        if(null !=savedInstanceState){
+            Log.w("CreateView", "  >(-^-)<  "+savedInstanceState.getInt(SCROLL_VALUE));
+            mScrollPositionY = savedInstanceState.getInt(SCROLL_VALUE);
+        }else {
+            Log.w("CreateView", "  >(-^-)<  "+"NULLLL");
+        }
         mWebView.loadDataWithBaseURL(mSiteUrl, mSiteData, "text/html", "UTF-8", null);
         mHistoryList.add(mSiteUrl);
+
         return rootView;
     }
 
@@ -63,6 +76,7 @@ public class WebViewFragment extends Fragment {
         mSiteData= data;
     }
 
+
     public boolean canGoPreviousPage(){
         return 1 < mHistoryList.size();
     }
@@ -70,14 +84,42 @@ public class WebViewFragment extends Fragment {
         mHistoryList.remove(mHistoryList.size() - 1);
         loadPage(mHistoryList.get(mHistoryList.size() - 1));
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SCROLL_VALUE, mWebView.getScrollY());
+//        outState.putFloat(SCROLL_VALUE, calcScrollPosition(mWebView));
+        Log.w("SAVEINST", "  >(-^-)<  " + mWebView.getScrollY());
+        Log.w("AFTERSAVE", String.valueOf(outState));
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.w("ONACTIVcrea", "call " + savedInstanceState);
+    }
     private class myWebViewClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             mWebViewBar = (ProgressBar) getActivity().findViewById(R.id.webViewBar);
+            Log.w("PageStart","  >(-^-)<  ");
         }
+
         @Override
         public void onPageFinished(WebView view, String url) {
             mWebViewBar.setVisibility(View.INVISIBLE);
+            Log.w("PageFinis", "  >(-^-)<  "+mScrollPositionY);
+            if (0 != mScrollPositionY){
+                view.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWebView.scrollTo(0,mScrollPositionY);
+                    }
+                }, 100);
+            }
+
+
+
         }
 
         @Override
@@ -96,6 +138,13 @@ public class WebViewFragment extends Fragment {
     private void loadPage (String url) {
         MinimizeHtmlPageTask minimizeHtmlPageTask = new MinimizeHtmlPageTask();
         minimizeHtmlPageTask.execute(url);
+    }
+
+    private float calcScrollPosition(WebView webView) {
+        float positionTopView = webView.getTop();
+        float contentHeight = webView.getContentHeight();
+        float currentScrollPosition = webView.getScrollY();
+        return (currentScrollPosition - positionTopView) / contentHeight;
     }
 
     private class MinimizeHtmlPageTask extends AsyncTask<String, Void, Void> {
